92cf5f2be53da31b7dd073990c6f85d1d1742ac4
Gulshan Raj <gulshanraj116@gmail.com>
2018-05-05 17:25:57 +0530

    convert upper case letters to lower case. Print spaces if more than one word.

--- bigfont.sh
+++ bigfont.sh
@@ -2,8 +2,15 @@
 read word
 for j in {1..5}; do
 for (( i=0; i<${#word}; i++ )); do
-letter=${word:i:1};
-sed -n "$j"p letters/$letter | tr -d '\n';
+character="${word:i:1}";
+#convert upper case to lower case and remove spaces
+letter="$(echo $character | tr '[A-Z]' '[a-z]')";
+letter=${letter// /};
+if [ "${#letter}" -eq 0 ]; then
+  printf "  ";
+else
+  sed -n "$j"p letters/$letter | tr -d '\n';
+fi
 printf " ";
 done;
 echo;
